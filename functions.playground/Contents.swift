f4237c701b73fa83e672fb25a5c675724a1420a8
Nagesh Aluri <111697399+Nageshaluri@users.noreply.github.com>
2023-02-23 15:10:29 -0600

    Add files via upload

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//1
+
+func greetUser(){
+    print("Welcome User")
+    
+}
+greetUser()
+
+//2
+
+func sayHello()->String{
+    var name="Swift"
+    return "Hello"+name+"!"
+}
+print(sayHello())
+
